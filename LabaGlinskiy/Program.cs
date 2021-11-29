51797331ac6474d525a4dbfc3d34f9817dad1617
Denis Denis <d_glinskiy@inbox.ru>
2021-11-29 22:26:32 +0300

    Добавьте файлы проекта.

---
+++ LabaGlinskiy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LabaGlinskiy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
